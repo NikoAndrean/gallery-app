9ce31752a5da39259ee8e2b1070f343ae5c986f3
NikoAndrean <blackmambaa404@gmail.com>
2023-06-15 10:18:32 +0700

    first commit

---
+++ src/Task.js
@@ -0,0 +1,5 @@
+const url = "YOURDEPLOYURL"; // TODO: replace this
+const studentName = "Muhammad Niko Andrean"; // TODO: replace this
+const studentId = "FE5438932"; // TODO: replace this
+
+export { url, studentName, studentId };
