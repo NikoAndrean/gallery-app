67a74b9db3c9063da0a632cc4a90235dea299a64
NikoAndrean <blackmambaa404@gmail.com>
2023-06-15 10:30:38 +0700

    add student name and student id

--- src/Task.js
+++ src/Task.js
@@ -1,4 +1,4 @@
-const url = "YOURDEPLOYURL"; // TODO: replace this
+const url = "https://galleryapp-fe5438932.netlify.app/"; // TODO: replace this
 const studentName = "Muhammad Niko Andrean"; // TODO: replace this
 const studentId = "FE5438932"; // TODO: replace this
 
